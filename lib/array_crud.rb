6880e1403c789a173a943afae0f96880c78a15f3
Erik Orozco <erikorozco1@gmail.com>
2020-07-15 10:15:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  name_of_family = ["erik", "isa", "adi", "nena"]
+  name_of_family.unshift("arrays!")
 end
 
 def remove_element_from_end_of_array(array)
